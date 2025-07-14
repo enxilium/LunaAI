4c03263935bf8d78cf242f7b12959e58bf36c312
Sukdip <sophieshu1480@gmail.com>
2025-07-14 15:05:33 -0400

    additional frontend push

--- src/renderer/components/main/PreferencesCard.tsx
+++ src/renderer/components/main/PreferencesCard.tsx
@@ -3,25 +3,28 @@ import React, { useState, useEffect } from "react";
 interface CardProps {
     header: string;
     description: string;
+    children?: React.ReactNode;
     onClick: () => void;
     disabled?: boolean;
     loading?: boolean;
     variant?: "primary" | "secondary";
 }
 
-const PreferencesCard: React.FC<CardProps> = ({header, description, onClick, disabled, loading, variant}) => {
+const PreferencesCard: React.FC<CardProps> = ({header, description, children, onClick, disabled, loading, variant}) => {
     return (
-        <div className="relative p-[2px] rounded-[18px] bg-gradient-to-b from-[rgb(128,126,181)] via-[rgb(48,28,94)] to-[rgb(84,53,154)] my-4">
-            <div className="flex flex-row bg-backgroundColor2 p-4 rounded-2xl shadow-md">   
-                <div className="flex flex-col w-full justify-center items-start gap-2">
-                    <div className="text-lg text-semibold text-accent">
+        <div className="relative p-[1px] rounded-[13px] bg-gradient-to-br from-[hsl(243,19%,37%)] via-[rgb(52,40,85)] to-[rgb(68,35,123)] my-4">
+            <div className="flex flex-row bg-backgroundColor2 p-3 rounded-[12px] shadow-md">   
+                <div className="flex flex-col w-full justify-center items-start gap-1">
+                    <div className="text-sm text-semibold text-accent">
                         {header}
                     </div>
-                    <div className="text-sm text-textColor">
+                    <div className="text-xs text-textColor">
                         {description}
                     </div>
                 </div>
-                <div className="flex flex-row w-full items-center justify-end">hi</div>
+                <div className="flex flex-row w-full items-center justify-end">
+                    {children}
+                </div>
             </div>
         </div>
     )
